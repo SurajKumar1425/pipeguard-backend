e5656405dd9efa9e08137009ca7f89a314794441
SurajKumar1425 <kumar813386@gmail.com>
2026-06-24 11:20:31 +0530

    fix cors

--- app.py
+++ app.py
@@ -157,17 +157,19 @@ app = FastAPI(
 # =========================
 
 app.add_middleware(
-
     CORSMiddleware,
 
-    allow_origins=["*"],
+    allow_origins=[
+        "https://pipeguard-dashboard.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
 
     allow_credentials=True,
 
     allow_methods=["*"],
 
     allow_headers=["*"]
-
 )
 
 # =========================
